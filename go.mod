1a54c4ec4942ae8ae909641edd3258fa1b365608
Sanket Pawar <sanketpawarsp7781@gmail.com>
2025-12-24 22:28:06 +0530

    chore: update Go version and add environment variable support with health check endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/sanketttt26/url-shortner
 
-go 1.23.5
+go 1.25.5
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("URL Shortner Using Go")
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/v1/health-check", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("Healthy"))
+	}).Methods("GET")
+
+	log.Println("Server running on port :", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
