1da7b1f32f618b98157080b321471442e1e23147
Sanket Pawar <sanketpawarsp7781@gmail.com>
2025-12-25 19:34:25 +0530

    feat: add Redis client creation function and update dependencies

--- api/database/database.go
+++ api/database/database.go
@@ -1,1 +1,32 @@
 package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func CreateClinet(redisDB int) *redis.Client {
+
+	redisAddr := os.Getenv("REDIS_ADDR")
+	if redisAddr == "" {
+		redisAddr = "localhost:6379"
+	}
+
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+
+	// Creating Redis client
+	RedisClient := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: redisPassword,
+		DB:       redisDB,
+	})
+
+	if err := RedisClient.Ping(context.Background()).Err(); err != nil {
+		log.Fatal("Redis connection failed:", err)
+	}
+
+	return RedisClient
+}
--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.25.5
 
 require github.com/joho/godotenv v1.5.1
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/redis/go-redis/v9 v9.17.2 // indirect
+)
